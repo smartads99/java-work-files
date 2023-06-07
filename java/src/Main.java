6900d9ae14c89a2cd214cb5745b6ef96a72ef8f7
Boobesh <boobeshsuresh16@gmail.com>
2023-06-07 22:16:02 +0530

    input

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("this the testing java out");
         System.out.println("First code in java");
+        System.out.println("Enter your numbers");
+        Scanner input= new Scanner(System.in);
+        System.out.println(input.nextLine());
     }
 }
--- java/src/demo.java
+++ java/src/demo.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class demo {
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("demo files print");
+        System.out.println("with the println");
+        System.out.print("i am from the without println");
+        System.out.print(" con .......");
+
+    }
 }
