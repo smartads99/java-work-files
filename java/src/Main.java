39c890d4c01c4700b4798374c8f7a5e5ac0d4d03
Boobesh <boobeshsuresh16@gmail.com>
2023-06-07 22:01:22 +0530

    move the files to git

---
+++ java/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this the testing java out");
+        System.out.println("First code in java");
+    }
+}
