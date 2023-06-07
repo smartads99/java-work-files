f020e5711313dfa6bb6997382ef7d30df232b9e9
Boobesh <boobeshsuresh16@gmail.com>
2023-06-07 21:44:22 +0530

    move the files to git

---
+++ java test/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+     public static void main(String[] args) {
+         System.out.println("Hello this is the java files");
+         System.out.println("with printls ");
+         System.out.print("this is without print");
+         System.out.print(" new from print");
+     }
+}
