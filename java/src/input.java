4a42fa215214fd84bd826b32ff97b7e95640e44c
Boobesh <boobeshsuresh16@gmail.com>
2023-06-08 21:30:22 +0530

    input with java

---
+++ java/src/input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the int");
+        int a=input.nextInt();
+        System.out.println("The numbers is " + a);
+
+
+    }
+}
