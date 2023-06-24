2452d830da084eadb7475d9c882e76a10004c620
Boobesh <boobeshsuresh16@gmail.com>
2023-06-24 22:38:59 +0530

    array

---
+++ array/src/array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int [][] arr={
+                {2,3,4,6},
+                {5},
+                {8,9,1},
+                {33,44,55}
+        };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j]= in.nextInt();
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
