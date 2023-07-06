1fb1ae0e72e03483cfcafcb68564b47bc43bb592
Boobesh <boobeshsuresh16@gmail.com>
2023-07-06 21:54:45 +0530

    max

--- binarysearch/src/rotationcount.java
+++ binarysearch/src/rotationcount.java
@@ -1,2 +1,17 @@
 public class rotationcount {
+    public static void main(String[] args) {
+        int [][] mat={
+                {2,3,4},
+                {2,3,5},
+                {22,33,44}
+        };
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print("[");
+            for (int j = 0; j < mat.length; j++) {
+                System.out.print(","+mat[i][j]);
+            }
+            System.out.print("]");
+            System.out.println();
+        }
+    }
 }
